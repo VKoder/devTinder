7c180bc466ffd70385649250761ca51b267d6490
VKoder <vivekkhule204@gmail.com>
2026-08-25 00:33:30 +0530

    refactor: restructure user route handling in app.js

--- src/app.js
+++ src/app.js
@@ -2,13 +2,28 @@ const express = require('express')
 
 const app = express();
 
-app.use('/hey', (req, res)=>{
-    res.send('This is running on hey path')
+// This will handle just get call to /user path
+app.get('/user', (req, res)=>{
+    res.send('Get user data')
 })
 
+app.post('/user', (req, res)=>{
+    // logic of posting data to db 
+    res.send('Post data to DB')
+})
+
+
+app.delete('/user', (req, res)=>{
+    // logic of deleting data from db 
+    res.send('Deleted data from DB')
+})
+
+
+// This will match all the HTTP method API calls to /test
 app.use('/test',(req, res)=>{
     res.send('This is running on /test path')
 })
+
 app.listen(7777, ()=>{
     console.log('The server is running on 7777 successfully...')
 })
