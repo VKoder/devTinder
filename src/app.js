effd505bbd2b45fe7be306e38b667b6c26811b6c
VKoder <vivekkhule204@gmail.com>
2026-08-29 23:34:41 +0530

    refactor: update error handling in app.js and clean up unused routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const express = require('express')
-const { adminAuth, userAuth } = require('./middlewares/auth')
-const app = express();
+const app = express()
 
-app.use('/admin', adminAuth)
-
-app.get('/admin', (req, res) => {
-    res.send('If user is authticated then only it will work')
+app.use('/', (req, res) => {
+    try {
+        throw new Error('Error')
+        res.send('Sent')
+    }
+    catch (err) {
+        res.status(500).send('Handled error')
+    }
 })
 
-app.use('/user', userAuth, (req, res) => {
-    console.log('This is user request')
-    res.send('Send the user data')
-})
 
+// can write like this in the end to handle the error but always use try catch
+app.use('/', (err, req, res, next)=>{
+    if(err){ 
+        res.status(500).send('Handled error')
+    }
+})
 app.listen(7777, () => {
-    console.log('The server is successfully running on 7777....')
+    console.log('Running successfully on port 7777....')
 })
